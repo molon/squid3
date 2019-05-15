e749e535e0554b8fc4854149db9988be3338d4c8
molon <dudl@qq.com>
2019-05-16 00:08:22 +0800

    高匿代理

---
+++ squid3_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/usr/bin/apt update
+/usr/bin/apt -y install apache2-utils squid3
+/usr/bin/htpasswd -b -c /etc/squid/passwd $SQUID_USER $SQUID_PASSWORD
+/bin/rm -f /etc/squid/squid.conf
+/usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/molon/squid3/master/squid.conf
+/sbin/iptables -I INPUT -p tcp --dport 8128 -j ACCEPT
+/sbin/iptables-save
+service squid restart
+update-rc.d squid defaults
