998dceba391e7ccb36cbfddb80707a37bc063ba0
molon <dudl@qq.com>
2019-05-16 00:42:11 +0800

    还是use 8128

--- squid3_install.sh
+++ squid3_install.sh
@@ -4,7 +4,7 @@
 /usr/bin/htpasswd -b -c /etc/squid/passwd $SQUID_USER $SQUID_PASSWORD
 /bin/rm -f /etc/squid/squid.conf
 /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/molon/squid3/master/squid.conf
-/sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+/sbin/iptables -I INPUT -p tcp --dport 8128 -j ACCEPT
 /sbin/iptables-save
 service squid restart
 update-rc.d squid defaults
